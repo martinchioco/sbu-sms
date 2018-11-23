8a37f11c4ad26a72b84f5bfde56b0878f18e635b
martinchioco <martinkurtbchioco@gmail.com>
2018-11-22 22:05:42 -0700

    Update deletefunc.php

--- adminindex/deletefunc.php
+++ adminindex/deletefunc.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
 $link = new mysqli('us-cdbr-iron-east-01.cleardb.net','bd7296d2ac0eff','4b840f06','heroku_a71bbafdab8fcb3');
-    $title = $_POST['title'];
     $projno = $_POST['projno'];
-    $qry="DELETE * FROM projects WHERE '$projno'=projno";
-    $qry="DELETE * FROM projinfo WHERE '$projno'=projno";
+    $qry="DELETE FROM projects WHERE '$projno'=projno";
+    $qry="DELETE FROM projinfo WHERE '$projno'=projno";
     mysqli_query($link,$qry);
     mysqli_query($link,$qry2);
     echo ("<script LANGUAGE='JavaScript'>
